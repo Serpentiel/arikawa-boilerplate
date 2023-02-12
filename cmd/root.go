8ee595dddd0d7e6125a0b3bb5b20a7aa9d4f0ae8
Aleksander Zaruczewski <zaruczewski@gmail.com>
2023-02-12 23:46:52 +0200

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+// Package cmd is the package that contains all of the commands for the application.
+package cmd
+
+import (
+	"os"
+
+	"github.com/Serpentiel/go-cli/internal/pkg/provide"
+	"github.com/spf13/cobra"
+	"go.uber.org/fx"
+)
+
+// rootCmd is the rootCmd command for the application.
+var rootCmd = &cobra.Command{
+	Use: "go-cli",
+	Run: func(cmd *cobra.Command, args []string) {
+		fx.New(
+			fx.Supply(cmd),
+
+			fx.Provide(
+				provide.Viper,
+				provide.Logger,
+			),
+
+			fx.WithLogger(provide.FxeventLogger),
+		).Run()
+	},
+}
+
+// Execute is the function that is called to execute the rootCmd command.
+func Execute() {
+	rootCmd.PersistentFlags().StringP("config", "c", "", "defines the config file to use")
+
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+// Package main is the package that contains the entry point for the application.
+package main
+
+import (
+	"github.com/Serpentiel/go-cli/cmd"
+)
+
+// main is the entry point for the application.
+func main() {
+	cmd.Execute()
+}
